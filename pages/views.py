6b355e5172b565693ea30403f92a68863dc44873
YangJiale <jlyang98@163.com>
2022-09-07 23:45:03 +0800

    YouTube Online Tutorial about Django start.

---
+++ pages/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    #print(args, kwargs)
+    #print(request.user)
+    return HttpResponse("<h1>Hello World</h1>") # string of HTML code
+
+def contact_view(request, * args, ** kwargs):
+    #print(args, kwargs)
+    return HttpResponse("<h1>Contact page</h1>")
+
+def template_view(request, *args, ** kwargs):
+    return render(request, "template.html", {})
